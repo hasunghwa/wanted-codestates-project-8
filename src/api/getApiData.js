4f7b13c328cd3fd22a03097356eaa93a9a0a27fb
DESKTOP-ICDGUQJ\하성화 <tjdghk3920@naver.com>
2022-03-24 21:03:59 +0900

    🐛 Fix : proxy url 변경

--- src/api/getApiData.js
+++ src/api/getApiData.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const getApiData = async (page) => {
   try {
     const res = await axios.get(
-      `/openapi-json/pubdata/pubMapForest.do?pageNo=${page}`
+      `https://www.chungbuk.go.kr/openapi-json/pubdata/pubMapForest.do?pageNo=${page}`
     );
     const parsedData = JSON.parse(res.data);
     return parsedData.response;
