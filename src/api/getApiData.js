b3e6141e450c77997a6c9e83d6fd83aa6b54fa97
hasunghwa <tjdghk3920@naver.com>
2022-03-25 15:43:35 +0900

    Update getApiData.js

--- src/api/getApiData.js
+++ src/api/getApiData.js
@@ -4,7 +4,7 @@ import { API_URL } from "../constants";
 export const getApiData = async (page) => {
   try {
     const res = await axios.get(
-      `http://apis.data.go.kr/6430000/cbRecreationalForestInfoService${API_URL}&perPage=10&currentPage=${page}`
+      `/api${API_URL}&perPage=10&currentPage=${page}`
     );
     return res.data.body;
   } catch (err) {
