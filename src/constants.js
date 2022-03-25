6d899bbc7622e514a3437942a6b12b8856d6fd58
hasunghwa <tjdghk3920@naver.com>
2022-03-25 15:15:30 +0900

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 const SERVICE_KEY = process.env.REACT_APP_SERVICE_KEY;
-export const API_URL = `/getRecreationalForestInfo?serviceKey=${SERVICE_KEY}`;
+export const API_URL = `/getRecreationalForestInfo?serviceKey=8UBV6FlfDpcGEbHNMcoCqEfgBbeaCoAvTDkSnJwzQiR7Qg8tIWaej%2FFX%2BwJL%2BDfZ%2Bb56dXnz6IpuFBM3evA%2F1w%3D%3D`;
